c412b50fed6247c5fd9a1469d58e282630d2e4ee
bbobox <gboka@yahoo.fr>
2018-02-19 00:51:57 +0100

    cleaning git project

--- src/vue/FenetrePricipale.java
+++ src/vue/FenetrePricipale.java
@@ -1,5 +1,16 @@
 package vue;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import client_server.KeyCommand;
+
+
+
 /**
  * 
  * Fenetre Principale d'utilisation du jeu Snake RPG
@@ -7,8 +18,47 @@ package vue;
  *
  */
 
-public interface FenetrePricipale {
-	
-	
 
+public class FenetrePricipale extends JFrame {
+    private static final long serialVersionUID = -5222658361778310082L;
+    private BorderLayout layout;
+ 
+    public FenetrePricipale() {
+        // création de la fenêtre
+        super("TestKeyListener");
+         
+        // ajout d'un seul composant dans cette fenêtre : un JLabel
+        JLabel label = new JLabel("Pressez une touche...");
+        add(label, BorderLayout.CENTER);
+         
+        // ajoute un écouteur d'événements personnalisé à la fenêtre
+        this.addKeyListener(new KeyCommand(label));
+         
+        // réglage des dimensions de la fenêtre
+        setPreferredSize(new Dimension(300, 100));
+        pack();
+        
+        
+        this.setLayout(layout);
+		this.setTitle("Snake RPG Multi Joueur");
+	    // Taille de la frame
+	    this.setSize(1024, 768);
+	    // Placer au centre de l'ecran
+	    this.setLocationRelativeTo(null);
+	    // Resizable ou non
+	    this.setResizable(true);
+        // Action a la fermeture (croix)
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        this.setVisible(true);
+    }
+     
+    public static void main(String[] args) {
+        // construit une fenêtre de type TestKeyListener et l'affiche
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new FenetrePricipale();
+                frame.setVisible(true);
+            }
+        });
+    }
 }
